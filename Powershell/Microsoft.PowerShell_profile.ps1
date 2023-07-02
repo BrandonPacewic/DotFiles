a54609cf3b9a7f63b335f133e8e300b2ec3d9f0a
Brandon P <pacewicbrandon@gmail.com>
2023-07-02 15:28:38 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,23 @@
 # Copyright (c) Brandon Pacewic
 # SPDX-License-Identifier: MIT
 
+# Enable git posh
+Import-Module posh-git
+
+# Enable git posh autocomplete
+function Set-PoshGitStatus {
+    $global:GitStatus = Get-GitStatus
+    $env:POSH_GIT_STRING = Write-GitStatus -Status $global:GitStatus
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshGitStatus' -Scope Global -Force
+
+$GitPromptSettings.DefaultPromptPath = ''
+$GitPromptSettings.DefaultPromptSuffix = ''
+
 # Shorten prompt to only show current directory
 function prompt {
-  $p = Split-Path -leaf -path (Get-Location)
-  "$p> "
+    $gitPrompt = & $GitPromptScriptBlock
+    $p = $gitPrompt
+    $p += Split-Path -leaf -path (Get-Location)
+    "$p> "
 }
